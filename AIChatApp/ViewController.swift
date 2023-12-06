defdd93cffd353b31066b1b8aba3749652082e35
Nesrin <nesrinnbozan6@gmail.com>
2023-12-06 14:01:26 +0300

    Initial Commit

---
+++ AIChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AIChatApp
+//
+//  Created by Nesrin Bozan on 6.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
