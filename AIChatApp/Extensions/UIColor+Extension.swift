a2fcffce36e5eeb031c8587db528e6adfd7acaaa
Nesrin <nesrinnbozan6@gmail.com>
2023-12-20 15:44:38 +0300

    emptyChatView added

--- AIChatApp/Extensions/UIColor+Extension.swift
+++ AIChatApp/Extensions/UIColor+Extension.swift
@@ -14,7 +14,7 @@ extension UIColor {
     static let whiteClr = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
     static let darkBlackClr = UIColor(red: 0.232, green: 0.232, blue: 0.232, alpha: 1)
     static let lightGreenClr = UIColor(red: 0.431, green: 0.749, blue: 0.353, alpha: 0.1)
-    static let grayClr = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).cgColor
+    static let grayClr = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3)
     static let lightGrayClr = UIColor(red: 0.322, green: 0.322, blue: 0.322, alpha: 1)
     static let lightGray = UIColor(red: 0.851, green: 0.851, blue: 0.851, alpha: 1)
     static let exploreLightGray = UIColor(red: 0.518, green: 0.518, blue: 0.518, alpha: 1)
--- AIChatApp/Scene/Tabbar/History/HistoryVC.swift
+++ AIChatApp/Scene/Tabbar/History/HistoryVC.swift
@@ -6,24 +6,47 @@
 //
 
 import UIKit
+import NeonSDK
 
 class HistoryVC: UIViewController {
 
+    private let emptyChatView = EmptyChatView()
+    let headerView = CustomHeaderView(pageTitle: "MathSolver & GPT".localized(), showLeftButton: false, showRightButton: true)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupView(){
+        view.backgroundColor = .whiteClr
+        
+        headerView.rightBtn.setImage(UIImage(named: "btn_settings"), for: .normal)
+        headerView.rightBtn.addTarget(self, action: #selector(rightButtonTapped), for: .touchUpInside)
+        
+        view.addSubview(headerView)
+        headerView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(70)
+            make.leading.equalTo(view.snp.leading)
+            make.trailing.equalTo(view.snp.trailing)
+            make.height.equalTo(60)
+        }
+        
+        emptyChatView.showInView(emptyChatView) {
+            self.present(destinationVC: SettingsVC(), slideDirection: .right)
+        }
+        
+        view.addSubview(emptyChatView)
+        emptyChatView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.height.equalTo(400)
+            make.left.right.equalToSuperview()
+       
     }
-    */
-
+    
+        
+    }
+    @objc func rightButtonTapped(){
+        present(destinationVC: SettingsVC(), slideDirection: .right)
+    }
 }
